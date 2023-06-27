79fd13fc1bdd76af972a969e335e083d8cd0d8f1
AllanChongNC <136596985+AllanChongNC@users.noreply.github.com>
2023-06-27 18:00:02 +0100

    Update ChatService.cs
    Added userID

--- Services/ChatService.cs
+++ Services/ChatService.cs
@@ -107,17 +107,18 @@ public class ChatService
     /// <summary>
     /// Get a completion from _openAiService
     /// </summary>
-    public async Task<string> GetChatCompletionAsync(string? sessionId, string prompt)
+    public async Task<string> GetChatCompletionAsync(string? sessionId, string? userID, string prompt)
     {
         ArgumentNullException.ThrowIfNull(sessionId);
+        ArgumentNullException.ThrowIfNull(userId);
 
-        Message promptMessage = await AddPromptMessageAsync(sessionId, prompt);
+        Message promptMessage = await AddPromptMessageAsync(sessionId, userID, prompt);
 
         string conversation = GetChatSessionConversation(sessionId);
 
         (string response, int promptTokens, int responseTokens) = await _openAiService.GetChatCompletionAsync(sessionId, conversation);
 
-        await AddPromptCompletionMessagesAsync(sessionId, promptTokens, responseTokens, promptMessage, response);
+        await AddPromptCompletionMessagesAsync(sessionId, userID, promptTokens, responseTokens, promptMessage, response);
 
         return response;
     }
@@ -168,9 +169,9 @@ public class ChatService
     /// <summary>
     /// Add user prompt to the chat session message list object and insert into the data service.
     /// </summary>
-    private async Task<Message> AddPromptMessageAsync(string sessionId, string promptText)
+    private async Task<Message> AddPromptMessageAsync(string sessionId, string userID, string promptText)
     {
-        Message promptMessage = new(sessionId, nameof(Participants.User), default, promptText);
+        Message promptMessage = new(sessionId, userID, nameof(Participants.User), default, promptText);
 
         int index = _sessions.FindIndex(s => s.SessionId == sessionId);
 
@@ -182,13 +183,13 @@ public class ChatService
     /// <summary>
     /// Add user prompt and AI assistance response to the chat session message list object and insert into the data service as a transaction.
     /// </summary>
-    private async Task AddPromptCompletionMessagesAsync(string sessionId, int promptTokens, int completionTokens, Message promptMessage, string completionText)
+    private async Task AddPromptCompletionMessagesAsync(string sessionId, string userID, int promptTokens, int completionTokens, Message promptMessage, string completionText)
     {
 
         int index = _sessions.FindIndex(s => s.SessionId == sessionId);
         
         //Create completion message, add to the cache
-        Message completionMessage = new(sessionId, nameof(Participants.Assistant), completionTokens, completionText);
+        Message completionMessage = new(sessionId, userID, nameof(Participants.Assistant), completionTokens, completionText);
         _sessions[index].AddMessage(completionMessage);
 
         
