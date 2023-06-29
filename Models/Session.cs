27686ddd8f93da5ec71374404f339ee4f5e5f81e
AllanChongNC <136596985+AllanChongNC@users.noreply.github.com>
2023-06-29 16:11:31 +0100

    Update Session.cs

--- Models/Session.cs
+++ Models/Session.cs
@@ -9,46 +9,7 @@ using System.Web;
 using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
 
-using System.Collections.Generic;
-using System.Security.Claims;
-using System.Text;
-using System.Text.Json;
-using Microsoft.AspNetCore.Http;
-
 namespace Cosmos.Chat.GPT.Models;
-
-public static class StaticWebAppsAuth
-{
-  private class ClientPrincipal
-  {
-      public string IdentityProvider { get; set; }
-      public string UserId { get; set; }
-      public string UserDetails { get; set; }
-      public IEnumerable<string> UserRoles { get; set; }
-  }
-  public static ClaimsPrincipal Parse(HttpRequest req)
-  {
-      var principal = new ClientPrincipal();
-      if (req.Headers.TryGetValue("x-ms-client-principal", out var header))
-      {
-          var data = header[0];
-          var decoded = Convert.FromBase64String(data);
-          var json = Encoding.UTF8.GetString(decoded);
-          principal = JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-      }
-      principal.UserRoles = principal.UserRoles?.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
-      if (!principal.UserRoles?.Any() ?? true)
-      {
-          return new ClaimsPrincipal();
-      }
-      var identity = new ClaimsIdentity(principal.IdentityProvider);
-      identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
-      identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
-      identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
-      return new ClaimsPrincipal(identity);
-  }
-}
-
 
 
 /// using(var context = new PrincipalContext(ContextType.Domain)) {};
@@ -97,7 +58,6 @@ public record Session
 
         ///HttpContext httpContext = HttpContext.Current;
         ///string authHeader = this.HttpContext.Request.Headers["Authorization"];
-        UserID = StaticWebAppsAuth;
         ///UserID = HttpContext.User.Identity.Name;
         ///Principal.Current.EmailAddress;
         ///UserID = System.Environment.UserName;
